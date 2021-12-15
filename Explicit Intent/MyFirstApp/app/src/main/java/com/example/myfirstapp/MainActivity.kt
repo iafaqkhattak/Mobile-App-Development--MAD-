0e1d6bf3acd0842917f1c41431ed02c95cb5d408
Muhammad Afaq <iafaqkhattak@gmail.com>
2021-12-15 21:34:28 +0500

    Explicit Intent Of Camera

--- Explicit Intent/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ Explicit Intent/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,9 +2,9 @@ package com.example.myfirstapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,18 +13,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.linear_layout)
         val  etUserMessage: TextView = findViewById(R.id.etUserMessage)
         val button1: Button= findViewById(R.id.btnSendMessageToNextActivity)
-         button1.setOnClickListener {
-
-         }
 
         button1.setOnClickListener {
-            val message: String = etUserMessage.text.toString()
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            /*val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show() */
+            /*val intent =Intent (this, Second_Activity::class.java)
+            startActivity(intent)*/
+            capturePhoto()
 
-            val intent =Intent (this, Second_Activity::class.java)
-            startActivity(intent)
         }
 
 
     }
+
+
+    fun capturePhoto() {
+        val intent = Intent(MediaStore.INTENT_ACTION_VIDEO_CAMERA)
+        if (intent.resolveActivity(packageManager) != null) {
+            val REQUEST_IMAGE_CAPTURE = 1
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+
+
+
 }
+
